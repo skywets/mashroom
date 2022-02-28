7433f28fa3f53580ba00642ac26073ca21391e22
ozodbek <ozodsamobahromov777@mail.ru>
2022-02-28 10:54:21 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,8 @@
 package com.company;
-import java.util.*;
-
-import static com.company.Stack.*;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-
-//
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter index of array which you want to remove?:  ");
-//        int index = scanner.nextInt();
-//
-        Stack1 test = new Stack1();
-        System.out.println();
-        for (int i = 0; i < 25; i++) {
-            test.push(i);
-        }
-        for (int i = 10; i < 16; i++) {
-            test.pop(i);
-            System.out.println(test.conversionToString());
-        }
-
-
-
-    }
-}
-//        int [] arr = {9,7,8,22,5,6,7};
-//        int len = remove( 5, arr);
-//        for (int i = 0; i < len; i++) {
-//            System.out.print(arr[i]+" ");
-
+        // write your code her
 
 
         // Mushroom mushroom = new Mushroom();
@@ -38,4 +10,5 @@ public class Main {
 //        mushroomPicker.addMushroom(new Forest());
 //        mushroomPicker.display();
 
-
+    }
+}
--- src/com/company/PseudoMap.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-
-public class PseudoMap {
-
-    Stack[] keyStack = (Stack[]) new Object();
-    Stack[] valueStack = (Stack[]) new Object();
-
-}
