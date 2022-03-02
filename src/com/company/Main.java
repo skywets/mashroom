6fc32ac7038477bd917651e57ddbe0340b3c53a4
ozodbek <ozodsamobahromov777@mail.ru>
2022-03-02 20:57:48 +0300

    commit3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.lang.management.MemoryUsage;
 import java.util.Random;
 
 public class Main {
@@ -7,16 +8,20 @@ public class Main {
     public static void main(String[] args) {
         // write your code her
 
+        MushroomPicker mushroomPicker = new MushroomPicker("Petya", 35);
+        MushroomPicker mushroomPicker1 = new MushroomPicker("Petya1", 35);
+        MushroomPicker mushroomPicker2 = new MushroomPicker("Petya2", 35);
+        for (int i = 0; i < 10; i++) {
+            mushroomPicker.collectMushroom(new Forest());
+            mushroomPicker1.collectMushroom(new Forest());
+            mushroomPicker2.collectMushroom(new Forest());
+        }
+        mushroomPicker.remove();
+        mushroomPicker1.remove();
+        mushroomPicker2.remove();
 
-Tournament tournament =new Tournament();
-tournament.point();
-//
-//        mushroomPicker.display();
-//        System.out.println();
-//        mushroomPicker1.display();
-//        System.out.println();
-//        mushroomPicker2.display();
-
+        Tournament tournament =new Tournament(mushroomPicker, mushroomPicker1, mushroomPicker2, new Forest());
+        tournament.compete();
 
     }
 }
